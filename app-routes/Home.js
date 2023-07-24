3bdbc1765ff6227779fb03835501268c8b175613
Al Fareed <76606638+Al-Fareed@users.noreply.github.com>
2023-07-24 15:16:12 +0530

    fetching units consumed by users in home

--- app-routes/Home.js
+++ app-routes/Home.js
@@ -1,9 +1,46 @@
 const express = require('express');
-const router = express.Router();
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+// Step 1: Create a schema for the power usage data
+const powerUsageSchema = new Schema({
+  year: {
+    type: Number,
+    required: true,
+  },
+  values: {
+    type: [Number],
+    required: true,
+  },
+});
 
-router.get('/',(req,res,next)=>{
-    console.log('Get Request working');
-    res.json({message:'Home page'})
+// Step 2: Create a schema for the user's power usage
+const userPowerUsageSchema = new Schema({
+  _id: {
+    type: String, // Assuming the userID is a string (e.g., "64bcb0e228523cd71278aa3b")
+    required: true,
+  },
+  powerUsage: {
+    type: [powerUsageSchema],
+    required: true,
+  },
+});
+
+// Step 3: Define a model based on the user power usage schema
+const UserPowerUsage = mongoose.model('power_usage', userPowerUsageSchema);
+
+const router = require('express').Router();
+
+// Step 4: Fetch the data from the database using the defined model
+router.get('/', (req, res, next) => {
+    console.log("start fetching ");
+  UserPowerUsage.find()
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => {
+      console.log('Error fetching data', err);
+      res.status(500).json({ error: 'Could not fetch data' });
+    });
 });
 
 module.exports = router;
--- index.js
+++ index.js
@@ -9,15 +9,15 @@ const app = express();
 app.use(bodyParser.json());
 
 // Attach routes to their endpoints
-app.use('/home', home);
 app.use('/buy', buy);
 app.use('/sell', sell);
+app.use('/', home);
 
 // Connect to the database and start the server
 mongoose.connect("mongodb+srv://AlFareed:Oyl1MxjZiH5FY96l@cluster0.urwkxxh.mongodb.net/soebms?retryWrites=true&w=majority")
   .then(() => {
     console.log('Connected to Database');
-    app.listen(5000);
+    app.listen(8000);
   })
   .catch((err) => {
     console.log("Error in connecting", err);
